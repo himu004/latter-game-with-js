713223d7042c8c4c6b38db4d8dbf695fc7892c43
Himu004 <himu.chowdhury01@gmail.com>
2024-09-17 21:09:35 +0600

    Updated

--- script/script.js
+++ script/script.js
@@ -1,41 +1,60 @@
+document.addEventListener("keyup", (event) => {
+  const playerPressed = event.key;
 
-document.addEventListener('keyup', (event) => {
-    const playerPressed = event.key;
+  // Get The pressed Latter
+let pointInt = 0;
+  const currentAlphabetElement = document.getElementById("text");
 
-    // Get The pressed Latter
-
-    const currentAlphabetElement = document.getElementById('text');
-
-    const currentAlpha = currentAlphabetElement.innerText.toLowerCase();
-
-    if (playerPressed === currentAlpha) {
-       console.log('Pointed');
-       removeBgById(currentAlpha)
-       continueGame();
-       
-    }
-    else{
-        console.log('lost');
-    }
-})
+  const currentAlpha = currentAlphabetElement.innerText.toLowerCase();
 
 
-function play(){
-    // Hide Home Screen
-    hideElementById('home-screen')
-    showElementById('play-ground')
-    // Game Loop
+  if (playerPressed === currentAlpha) {
+    const point = document.getElementById("point");
+    pointInt = parseInt(point.innerText);
+    pointInt = pointInt + 1;
+    point.innerText = pointInt;
+
+    //    New Round
+    removeBgById(currentAlpha);
     continueGame();
-   
+  } else {
+    const life = document.getElementById("life");
+    let lifeInt = parseInt(life.innerText);
+    lifeInt = lifeInt - 1;
+
+    life.innerText = lifeInt;
+
+    if (lifeInt <= 0) {
+        document.getElementById('text').innerText = 'Game Over';
+        life.innerText = 0;
+    }
+    
+  }
+});
+
+function play() {
+  // Hide Home Screen
+  hideElementById("home-screen");
+  showElementById("play-ground");
+  
+//   const life = document.getElementById("life");
+//   life.innerText = 5;
+//   const point = document.getElementById("point");
+//   point.innerText = 0;
+  
+
+  // Game Loop
+  continueGame();
 }
 // Hide Play Screen
-function score(){
-    hideElementById('play-ground')
-    showElementById('score-screen')
+function score() {
+  hideElementById("play-ground");
+  showElementById("score-screen");
+  const totalPoint = document.getElementById("total-point");
+  totalPoint.innerText = point.innerText
+  
 }
-
-
-// Reload The Whole Game
-function reloadPage() {
+// Play Again 
+function playAgain() {
     location.reload();
-  }
+}
